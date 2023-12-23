61e7bb5491e24fd305b3aa3d4a71270ce7275ebc
Mohit Yadav <my701319@gmail.com>
2023-12-23 16:40:18 +0530

    46th commit

--- src/functions/onMobile.js
+++ src/functions/onMobile.js
@@ -6,8 +6,8 @@ function onMobile() {
 function url() {
   // return `http://localhost:3000`;
   // return     `https://shopon-backend-production.up.railway.app`
-  // return `https://shopon-0ez5.onrender.com`;
-  return `https://shopon.up.railway.app`;
+  return `https://shopon-auc9.onrender.com`;
+  // return `https://shopon.up.railway.app`;
 }
 
 export { onMobile, url };
