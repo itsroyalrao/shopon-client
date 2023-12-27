0d8792a105a6e027899dae8df192da0be66eafae
Mohit Yadav <my701319@gmail.com>
2023-12-27 11:40:47 +0530

    49th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux";
-
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import Orders from "./pages/Orders";
@@ -10,27 +8,24 @@ import UpdateProduct from "./pages/UpdateProduct";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 // import ContactUs from "./pages/ContactUs";
-import store from "./app/store.js";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <div className="flex flex-col w-full min-h-[100svh] bg-[#242424] text-white relative">
-          <Routes>
-            <Route path="/signup" element={<Signup />} />
-            <Route path={"/login"} element={<Login />} />
-            <Route path={"/"} element={<Products />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/admin/add-product" element={<AddProduct />} />
-            <Route path="/admin/products" element={<AdminProducts />} />
-            {/* <Route path="/contact-us" element={<ContactUs />} /> */}
-            <Route path={`/update-product/:id`} element={<UpdateProduct />} />
-          </Routes>
-        </div>
-      </Router>
-    </Provider>
+    <Router>
+      <div className="flex flex-col w-full min-h-[100svh] bg-[#242424] text-white relative">
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/"} element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/admin/add-product" element={<AddProduct />} />
+          <Route path="/admin/products" element={<AdminProducts />} />
+          {/* <Route path="/contact-us" element={<ContactUs />} /> */}
+          <Route path={`/update-product/:id`} element={<UpdateProduct />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/functions/onMobile.js
+++ src/functions/onMobile.js
@@ -5,9 +5,9 @@ function onMobile() {
 
 function url() {
   // return `http://localhost:3000`;
-  // return     `https://shopon-backend-production.up.railway.app`
-  return `https://shopon-auc9.onrender.com`;
+  // return `https://shopon-auc9.onrender.com`;
   // return `https://shopon.up.railway.app`;
+  return `https://shopon-backend.vercel.app`;
 }
 
 export { onMobile, url };
